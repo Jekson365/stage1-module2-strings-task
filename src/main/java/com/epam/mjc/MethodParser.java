5dda7c7c5dad75bb2b0306629bfc13046eb1690f
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-08-01 20:28:08 +0400

    kj

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -23,61 +23,22 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        StringBuilder sb = new StringBuilder(signatureString);
-        List<MethodSignature.Argument> res = new ArrayList<>();
 
+            MethodSignature m = new MethodSignature("");
+            String[] arr = signatureString.split(" ");
 
-        String[] sliced_part_1 = sb.substring(0,sb.indexOf("(")).split(" ");
-        String[] sliced_part_2 = sb.substring(sb.indexOf("("),sb.length()).split(",");
+            // set access modifier
+            m.setMethodName(arr[2].replace("()",""));
+            m.setAccessModifier(arr[0]);
+            m.setReturnType(arr[1]);
 
-        // results
-        String accessModifier = sliced_part_1[0];
-        String returnType = sliced_part_1[1];
-        String methodName  = sliced_part_1[2];
-        MethodSignature methodSignature = new MethodSignature(methodName,res);
-        System.out.println(accessModifier);
-        System.out.println(returnType);
-        System.out.println(methodName);
-        for (int i = 1; i < sliced_part_2.length - 1;i++) {
+            return m;
 
-            MethodSignature.Argument arg3 = new MethodSignature.Argument(
-                    sliced_part_2[i].split(" ")[0],
-                    sliced_part_2[i].split(" ")[1]
-            );
-            res.add(arg3);
         }
-
-        // create arguments list
-        MethodSignature m = new MethodSignature("dsad",res);
-
-        String slicedFirst = sliced_part_2[0].replace("(","");
-        String slicedLast = sliced_part_2[sliced_part_2.length - 1].replace(")","");
-
-        // first type and name;
-        String firstType = slicedFirst.split(" ")[0];
-        String firstName = slicedFirst.split(" ")[1];
-
-        // last type and name;
-        String lastType = slicedLast.split(" ")[0];
-        String lastName = slicedLast.split(" ")[1];
-
-        MethodSignature.Argument arg = new MethodSignature.Argument(firstType,firstName);
-        MethodSignature.Argument arg2 = new MethodSignature.Argument(lastType,lastName);
-
-        res.add(arg);
-        res.add(arg2);
-
-        methodSignature.setMethodName(methodName);
-        methodSignature.setReturnType(returnType);
-        methodSignature.setAccessModifier(accessModifier);
-
-
-        return methodSignature;
-    }
 
     public static void main(String[] args) {
         MethodParser m = new MethodParser();
-        m.parseFunction("private void log(String logString, LogLevel level, Context context)");
+        m.parseFunction("private void log(String logString, LogLevel level,Context context)");
     }
 
 
